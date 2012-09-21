67ed2bfbde5c824c5da1986377ed14bd03d93364
shaneflooks <shaneflooks@gmail.com>
2012-09-21 00:31:54 -0300

    First commit fisrt upload
    Host Your Own Webapp, Personal Nameplate, or Personal Website For Free
    in Seconds at Github

---
+++ conf/config.php
@@ -0,0 +1,27 @@
+<?php
+
+/*** VISUAL STYLE ***/
+$visual_style['background_image'] = 'http://shaneflooks.com/images/backgrounds/DSC05016.jpg'; // Enter any full URL to a photo to use it as your background photo
+$visual_style['navigation_shadows'] = true; // Add shadows to your name and the navigation elements to make them easier to read on complex background images
+$visual_style['navigation_color'] = '#FFFFFF'; // Set this to any HEX color value to change the color of your name and navigation
+
+/*** ABOUT ME ***/
+$general['first_name'] = 'Shane'; // Just your first name
+$general['last_name'] = 'Flooks'; // Just your last name
+$general['about_me'] = "Development / Programming, Web Systems Administration (Apache, Linux, Unix, MySQL, PHP), Database Design and Query Optimization, Organic SEO (Search Engine Optimization), Online Business Development, Marketing and Market Research, Technical and Creative Writing. Maintain and improve network infrastructure including servers, desktops, and wiring"; // This is where you can type information about yourself.  If you use any quotation marks, be sure to put a \ before them.
+
+/*** PHOTOS ***/
+$general['about_photos'] = 'These are my awesome Flickr photos!'; // This is description that shows up in your photos section
+$accounts['flickr']['username'] = 'shaneflooks'; // The username in your URL, if your photo URL is http://flickr.com/photos/lifehacker your username would be lifehacker
+$accounts['flickr']['apikey'] = '78710ac26bd00524b3d2075f83ded242'; // Looks like dfc510ae6d2c45d5fdf9259415pe7c92, and you can sign up for one (if you don't have one already) at http://www.flickr.com/services/apps/create/apply
+
+
+/*** VIDEOS ***/
+$general['about_videos'] = 'These are my awesome Vimeo and YouTube videos!'; // This is a description of
+$accounts['vimeo']['username'] = ''; // Entering your Vimeo username will display your public Vimeo videos on your page
+$accounts['youtube']['username'] = 'shaneflooks'; // Entering your YouTube username will display your public YouTube videos on your page
+
+/*** TWITTER ***/
+$accounts['twitter']['username'] = ''; // Entering your Twitter username will display your latest tweets on your page
+
+?>
